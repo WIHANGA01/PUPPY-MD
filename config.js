268880698030b6a03a3ef367a8ec214708ef1335
WIHANGA01 <wijerathnaattanayaka@gmail.com>
2024-11-13 14:16:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "3dUChbAJ#PSSpXkkX2ZcTc1Jl5W_NfK7-5IpbakerqTpe-odvvsU",
+SESSION_ID: process.env.SESSION_ID || "mIt0WBCZ#URBceh0H31fWcM5XOH6LxIH9ScmkOXeclN6NQRJSQTc",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://github.com/user-attachments/assets/514d5563-2106-4b8b-a6ce-48a005b194f6",
 ALIVE_MSG : process.env.ALIVE_MSG || "*PUPPY-MD ALIVE NOW*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
